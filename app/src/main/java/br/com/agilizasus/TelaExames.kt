34b164af34928e94194310c318fb36e0a97486d4
Aline Sayuri Kadekaro <aline.ask@gmail.com>
2023-11-23 00:28:10 -0300

    revisao final

--- app/src/main/java/br/com/agilizasus/TelaExames.kt
+++ app/src/main/java/br/com/agilizasus/TelaExames.kt
@@ -1,8 +1,6 @@
 package br.com.agilizasus
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 
 class TelaExames : ComponentActivity() {
--- app/src/main/java/br/com/agilizasus/TelaLogin.kt
+++ app/src/main/java/br/com/agilizasus/TelaLogin.kt
@@ -12,18 +12,21 @@ class TelaLogin : ComponentActivity() {
         setContentView(R.layout.tela_login)
 
     val btnsitegov : Button = findViewById(R.id.btn_sitegov)
+
         val btnprimeirocadastro : Button = findViewById(R.id.btn_primeirocadastro)
 
 
         btnsitegov.setOnClickListener{
 
-            val i = Intent(Intent.ACTION_VIEW, Uri.parse( uriString:, "https://sso.acesso.gov.br/login?client_id=www.gov.br&authorization_id=18beac91490"))
-            startActivity(i)
-        }
+
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://sso.acesso.gov.br/login?client_id=www.gov.br&authorization_id=18beac91490"))
+                startActivity(i)
+    }
+
 
         btnprimeirocadastro.setOnClickListener{
-            val i = Intent(this,TelaCadastro1::class.java)
-            startActivity(i)
+            val intent = Intent(this,TelaCadastro1::class.java)
+            startActivity(intent)
         }
     }
 }
